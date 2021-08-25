1cfa40229113f6759d0a0a7138ee08cf0573c73a
amrit1951 <amritshaw198@gmail.com>
2021-08-25 11:38:17 -1200

    task2 completed

--- bin/task2/src/task2.dart
+++ bin/task2/src/task2.dart
@@ -10,8 +10,17 @@ class Task2{
   //Write your login in getMinMax()
   //consider for this array [-1, -10, 90, 7, 0, 78, 1456]
   getMinMax(List<int>arr){
-    //todo
-    
+    this.max = arr[0];
+    this.min = arr[0];
+
+    for(int i=0;i<arr.length;i++){
+      if(arr[i]>max){
+        this.max = arr[i];
+      }else if(arr[i]< min){
+      this.min = arr[i];
+      }
+    }
+        
   }
 
   // do not edit this
